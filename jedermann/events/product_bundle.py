c3ccf31e73d955dbdd982c5172fd08a7cbac05cf
beingeek <furqan.790@outlook.com>
2025-01-17 12:27:38 +0000

    fix: message updated

--- jedermann/events/product_bundle.py
+++ jedermann/events/product_bundle.py
@@ -4,4 +4,4 @@ from frappe import _
 
 def validate_left_right_pair_item(doc, method=None):
     if doc.custom_is_left_right_pair_item and len(doc.items) > 2:
-        frappe.throw(_("Product Bundle checked with 'Is Left Right Pair Item', must contain only two List items in the package."))
+        frappe.throw(_("Product Bundle is checked with 'Is Left Right Pair Item', it must contain only two List items in the package."))
