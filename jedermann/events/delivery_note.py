ead256a05fea1ee20e02d7fd2895370911ab1fa3
beingeek <furqan.790@outlook.com>
2025-01-22 15:29:48 +0000

    feat: added functionality to set batch and stock entry for packed items

--- jedermann/events/delivery_note.py
+++ jedermann/events/delivery_note.py
@@ -2,8 +2,7 @@ import frappe
 from frappe import _
 from erpnext.stock.doctype.delivery_note.delivery_note import DeliveryNote
 from erpnext.stock.doctype.batch.batch import get_batch_qty
-from erpnext.stock.doctype.stock_entry.stock_entry_utils import make_stock_entry
-from jedermann.events.utils import validate_and_configure_left_right_pair_packed_item
+from jedermann.events.utils import validate_and_configure_left_right_pair_packed_item, is_product_bundle
 
 
 class CustomDeliveryNote(DeliveryNote):
@@ -13,6 +12,21 @@ class CustomDeliveryNote(DeliveryNote):
                 return
 
             for item in self.items:
+                if item.batch_no and item.warehouse and item.item_code and not is_product_bundle(item.item_code):
+                    qty = get_batch_qty(item.batch_no, item.warehouse, item.item_code)
+                    if qty < item.qty:
+                        qty = item.qty - qty
+                        stock_entry = make_stock_entry(
+                            item_code=item.item_code,
+                            qty=qty,
+                            warehouse=item.warehouse,
+                            batch_no=item.batch_no,
+                            dn=self.name
+                        )
+                    item.actual_batch_qty = qty
+            self.set_actual_qty()
+
+            for item in self.packed_items:
                 if item.batch_no and item.warehouse and item.item_code:
                     qty = get_batch_qty(item.batch_no, item.warehouse, item.item_code)
                     if qty < item.qty:
@@ -20,12 +34,11 @@ class CustomDeliveryNote(DeliveryNote):
                         stock_entry = make_stock_entry(
                             item_code=item.item_code,
                             qty=qty,
-                            to_warehouse=item.warehouse,
+                            warehouse=item.warehouse,
                             batch_no=item.batch_no,
-                            purpose="Material Receipt",
+                            dn=self.name
                         )
-                        stock_entry.add_comment("Comment", _("Adusting stock for Delivery Note {0}").format(self.name))
-            self.set_actual_qty()
+                    item.actual_batch_qty = qty
 
         self.set_batches_in_items()
         super().validate()
@@ -37,6 +50,10 @@ class CustomDeliveryNote(DeliveryNote):
             return
 
         for item in self.items:
+            if item.custom_batch_number and item.custom_batch_number != item.batch_no and not is_product_bundle(item.item_code):
+                item.batch_no = get_batch_no_from_custom_batches(item)
+
+        for item in self.packed_items:
             if item.custom_batch_number and item.custom_batch_number != item.batch_no:
                 item.batch_no = get_batch_no_from_custom_batches(item)
 
@@ -51,3 +68,20 @@ def get_batch_no_from_custom_batches(item):
         batch_name = batch.name
 
     return batch_name
+
+
+def make_stock_entry(item_code, qty, warehouse, batch_no, dn):
+    se = frappe.new_doc("Stock Entry")
+
+    se.stock_entry_type = "Material Receipt"
+    se.append("items", {
+        "item_code": item_code,
+        "qty": qty,
+        "t_warehouse": warehouse,
+        "use_serial_batch_fields": 1,
+        "batch_no": batch_no,
+    })
+    se.submit()
+    se.add_comment("Comment", _("Adjusting stock for Delivery Note {0}").format(dn))
+
+    return se
